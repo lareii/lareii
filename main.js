882ad1ede8e6ef033eb36ae9f30c69c32a84ca69
emirhan <72984140+lareii@users.noreply.github.com>
2023-12-03 10:03:35 +0000

    validating with sha256

--- main.js
+++ main.js
@@ -2,9 +2,54 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const app = express()
- 
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+let encoder = new TextEncoder();
+
+async function verifySignature(secret, header, payload) {
+    let parts = header.split("=");
+    let sigHex = parts[1];
+
+    let algorithm = { name: "HMAC", hash: { name: 'SHA-256' } };
+
+    let keyBytes = encoder.encode(secret);
+    let extractable = false;
+    let key = await crypto.subtle.importKey(
+        "raw",
+        keyBytes,
+        algorithm,
+        extractable,
+        [ "sign", "verify" ],
+    );
+
+    let sigBytes = hexToBytes(sigHex);
+    let dataBytes = encoder.encode(payload);
+    let equal = await crypto.subtle.verify(
+        algorithm.name,
+        key,
+        sigBytes,
+        dataBytes,
+    );
+
+    return equal;
+}
+
+function hexToBytes(hex) {
+    let len = hex.length / 2;
+    let bytes = new Uint8Array(len);
+
+    let index = 0;
+    for (let i = 0; i < hex.length; i += 2) {
+        let c = hex.slice(i, i + 2);
+        let b = parseInt(c, 16);
+        bytes[index] = b;
+        index += 1;
+    }
+
+    return bytes;
+}
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -13,7 +58,8 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
     let data = req.body;
     res.send('Data Received: ' + JSON.stringify(data));
-    console.log('Data Received: ', data);
+    console.log('Data Received. Validating...');
+    console.log('Valid: ' + verifySignature('test', req.headers['X-Hub-Signature-256'], JSON.stringify(data)));
 })
 
 app.listen(3000, () => {
